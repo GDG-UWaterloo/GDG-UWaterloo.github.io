d9c851f1613e67911b28b3d622cadb782f1da5d7
Pavel Shering <pavel.shering@hotmail.ca>
2016-04-13 03:49:49 -0400

    sidenav is almost wokring

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 'use strict'
+$(document).ready(function(){
+        // $("[data-activates=mobile-view]").sideNav();
+        $(".button-collapse").sideNav({menuWidth: 300});
+        // $('.parallax').parallax();
+        // $(".dropdown-button").dropdown({
+        //     hover: false
+        });
 
 var myApp = angular.module('GDG_UW', ['ngRoute'], function ($locationProvider) {
   // // Removes hash in url but cannot be used with Github Pages
@@ -6,7 +13,6 @@ var myApp = angular.module('GDG_UW', ['ngRoute'], function ($locationProvider) {
 });
 
 myApp.config(['$routeProvider', function ($routeProvider, $locationProvider) {
-
     $routeProvider
       .when('/', {
         controller: 'homeCtrl',
